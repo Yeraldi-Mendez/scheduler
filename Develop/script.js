4b415ba3a0435984a025205bbed6b90df803eb9c
Yeraldi-Mendez <yeraldimendez.ym@gmail.com>
2023-01-31 12:44:35 -0800

    added first part to the js file, commented as #1 TODO

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+//$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,15 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+//});
+
+var today = moment();
+
+$(document).ready(function(){
+  $("#currentDay").text(moment().format("dddd, MMMM"));
+  $(".saveBtn").on("click",function(){
+    var text = $(this).sibilings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+  })
+})
